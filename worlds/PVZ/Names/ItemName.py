dba84df519ad87392213097d07d527556d5d96cf
Chrono41111 <126972031+Chrono41111@users.noreply.github.com>
2023-03-06 11:23:21 -0500

    Add files via upload

---
+++ worlds/PVZ/Names/ItemName.py
@@ -0,0 +1,62 @@
+# Day Plants
+peashooter = 'Pea Shooter'
+sunflower = 'Sun Flower'
+cherry_bomb = 'Cherry Bomb'
+wall_nut = 'Wall-nut'
+potato_mine = 'Potato Mine'
+snow_pea = 'Snow Pea'
+chomper = 'Chomper'
+repeater = 'Repeater'
+
+# Night Plants
+puff_shroom = 'Puff-Shroom'
+sun_shroom = 'Sun-Shroom'
+fume_shroom = 'Fume-Shroom'
+grave_buster = 'Grave Buster'
+hypno_shroom = 'Hypno-Shroom'
+scaredy_shroom = 'Scaredy-Shroom'
+ice_shroom = "Ice-Shroom"
+doom_shroom = 'Doom-Shroom'
+
+# Pool Plants
+lily_pad = 'Lily Pad'
+squash = 'Squash'
+threepeater = 'Threepeater
+tangle_kelp = 'Tangle Kelp'
+jalapeno = "Jalapeno"
+spikeweed = 'Spikeweed'
+torchwood = 'Torchwood'
+tall_nut = 'Tall-nut'
+
+# Fog Plants
+sea_shroom = 'Sea-shroom'
+plantern = 'Plantern'
+cactus = 'Cactus'
+blover = 'Blover'
+split_pea = 'Split Pea'
+starfuit = 'Starfruit'
+pumpkin = 'Pumpkin'
+magnet_shroom = 'Magnet-shroom'
+
+# Roof Plants
+cabbage_pult = 'Cabbage-pult'
+flower_pot = 'Flower Pot'
+kernel_pult = 'Kernel-pult'
+coffee_bean = 'Coffee Bean'
+garlic = 'Garlic'
+umbrella_leaf = 'Umbrella Leaf'
+marigold = 'Marigold'
+melon_pult = 'Melon-pult'
+
+# Upgrade Plants
+gatling_pea = "Gatling Pea"
+twin_sunflower = 'Twin Sunflower'
+gloom_shroom = 'Gloom-shroom'
+cattail = 'Cattail'
+winter_melon = 'Winter Melon'
+gold_magnet = 'Gold Magnet'
+spikerock = 'Spikerock'
+cob_cannon = 'Cob Cannon'
+
+# Other Plants
+imitator = 'Imitator'
---
+++ worlds/PVZ/Names/LocationName.py
@@ -0,0 +1,4 @@
+# Stage Definitions
+
+# Day
+day 
